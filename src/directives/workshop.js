934c1039d83cf67e1062eb87cee6dfdb6fbc238e
Frederic Junod <frederic.junod@camptocamp.com>
2016-12-08 09:34:56 +0100

    ngeo workshop: step 3 (recenter on coordinate)

--- src/directives/workshop.js
+++ src/directives/workshop.js
@@ -16,8 +16,18 @@ ngeo.module.directive('ngeoWorkshop', ngeo.workshopDirective);
 
 
 ngeo.WorkshopController = function() {
+  this.coordinates = [
+    [730556, 5863720],
+    [829500, 5933600],
+    [950000, 6002000]
+  ];
+};
 
 
+ngeo.WorkshopController.prototype.recenter = function(coordinate) {
+  var view = this.map.getView();
+  view.setCenter(coordinate);
+  view.setZoom(10);
 };
 
 ngeo.module.controller('ngeoWorkshopController', ngeo.WorkshopController);
