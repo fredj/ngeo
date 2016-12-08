bca6c09a322e97414c81ee0dd4307782b2fe07a8
Frederic Junod <frederic.junod@camptocamp.com>
2016-12-08 09:15:19 +0100

    ngeo workshop: step 2 (controller and map property)

--- src/directives/workshop.js
+++ src/directives/workshop.js
@@ -3,8 +3,21 @@ goog.provide('ngeo.workshop');
 
 ngeo.workshopDirective = function() {
   return {
+    scope: {
+      'map': '<'
+    },
+    bindToController: true,
+    controller: 'ngeoWorkshopController as ctrl',
     templateUrl: ngeo.baseTemplateUrl + '/workshop.html'
   };
 };
 
 ngeo.module.directive('ngeoWorkshop', ngeo.workshopDirective);
+
+
+ngeo.WorkshopController = function() {
+
+
+};
+
+ngeo.module.controller('ngeoWorkshopController', ngeo.WorkshopController);
